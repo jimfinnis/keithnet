02cd507e9ea3b32a77b32cdc11baf881f066300f
Jim Finnis <jim.finnis@gmail.com>
2016-03-16 15:44:46 +0000

    Initial commit

---
+++ src/keithnet_node.cpp
@@ -0,0 +1,99 @@
+/**
+ * @file keithnet_node.cpp
+ * @brief Assumes a UESMANN net has been trained, with
+ * NUMSONARS (3) inputs, HNODES hidden layers and 2 outputs.
+ * The inputs are sonars, the outputs are left and right motors.
+ * 
+ * Reads the network from the file in ~netfile. No topology checks
+ * are done to make sure the read network is the same!
+ *
+ */
+
+#include "ros/ros.h"
+#include "std_msgs/Float32.h"
+#include "std_msgs/Float32MultiArray.h"
+
+#include "backpropNoBiasHormone.h"
+
+#define NUM_SONARS 3
+
+static float sonars[NUM_SONARS];
+void sonarCallback(const std_msgs::Float32MultiArray::ConstPtr &msg){
+    for(int i=0;i<NUM_SONARS;i++)
+        sonars[i] = msg->data[i];
+}
+
+int main(int argc,char *argv[]){
+    ros::init(argc,argv,"keithnet_node");
+    ros::NodeHandle n;
+    
+    // set up the network - we assume a certain geometry
+    // which must match up!
+    
+    // 3 inputs, some hidden nodes, 2 outputs
+    int layers[]={NUM_SONARS,10,2};
+    BackpropNetNoBiasHormone net(1); // eta is irrelevant
+    net.init(3,layers);
+    
+    // try to read the genome file
+    std::string file;
+    if(!ros::param::get("~netfile",file)){
+        ROS_ERROR("~netfile not specified");
+        exit(0);
+    }
+    
+    FILE *a;
+    if(!(a=fopen(file.c_str(),"rb"))){
+        ROS_ERROR("cannot open netfile %s",file.c_str());
+        exit(0);
+    }
+    
+    int gs = net.getGenomeSize();
+    double *qq = new double[gs];
+    size_t r = fread(qq,sizeof(double),gs,a);
+    fclose(a);
+    if(r!=sizeof(double)*gs){
+        ROS_ERROR("Netfile %s too short",file.c_str());
+        exit(0);
+    }
+    net.setFromGenome(qq);
+    delete [] qq;
+    
+    
+    // subscribe to sonar topics
+    ros::Subscriber sonarSub =
+          n.subscribe<std_msgs::Float32MultiArray>("sonar",100,sonarCallback);
+    
+    // publish to motor topics
+    ros::Publisher leftMotorPub =
+          n.advertise<std_msgs::Float32>("leftmotors",100);
+    ros::Publisher rightMotorPub =
+          n.advertise<std_msgs::Float32>("rightmotors",100);
+    
+    
+    ros::Rate rate(10);
+    while(ros::ok()){
+        ros::spinOnce();
+        
+        // feed the network
+        double ins[NUM_SONARS];
+        for(int i=0;i<NUM_SONARS;i++)
+            ins[i]=sonars[i];
+        
+        net.setH(0);
+        net.setInputs(ins);
+        net.update();
+        
+        // update the robot
+        double *outs = net.getOutputs();
+        std_msgs::Float32 msg;
+        msg.data=outs[0];
+        leftMotorPub.publish(msg);
+        msg.data=outs[1];
+        leftMotorPub.publish(msg);
+        
+        rate.sleep();
+    }
+    
+    return 0;
+}
